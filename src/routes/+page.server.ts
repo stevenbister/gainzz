958bffb9ba6d440eecda4bdd279cf4f87271bd1d
Steven Bister <stevebister@gmail.com>
2024-03-30 13:21:57 +0000

    feat: add util to get user data

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,19 +1,22 @@
+import { getUser } from '$lib/utils';
+import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
-export const load: PageServerLoad = async ({
-	locals: {
-		supabase,
-		// getSession
-	},
-}) => {
-	// const session = await getSession()
+export const load: PageServerLoad = async ({ locals: { supabase, getSession } }) => {
+	const session = await getSession();
 
-	// if (!session) {
-	//     throw redirect(303, '/')
-	//   }
+	if (!session) {
+		throw redirect(303, '/login');
+	}
+
+	const user = await getUser(supabase);
 
 	// Test data is getting returned
-	const { data: profile } = await supabase.from('user_profile').select('*').eq('id', 1).single();
+	const { data: profile } = await supabase
+		.from('user_profile')
+		.select('*')
+		.eq('user_id', user.id)
+		.single();
 
 	return { profile };
 };
