4413068e4fc25373e2938e0172371c6ab9806890
Steven Bister <stevebister@gmail.com>
2024-04-07 11:09:26 +0100

    feat: adds workout tables

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -93,3 +93,136 @@ insert into public.user_role (
     (select id from auth.users where instance_id = '00000000-0000-0000-0000-000000000000'),
     (select id from public.role where role_name = 'admin')
 );
+
+insert into public.exercise (
+    name,
+    created_by
+) values (
+    'Squat',
+    (select id from auth.users where instance_id = '00000000-0000-0000-0000-000000000000')
+), (
+    'Bench Press',
+    (select id from auth.users where instance_id = '00000000-0000-0000-0000-000000000000')
+), (
+    'Deadlift',
+    (select id from auth.users where instance_id = '00000000-0000-0000-0000-000000000000')
+), (
+    'Shoulder Press',
+    (select id from auth.users where instance_id = '00000000-0000-0000-0000-000000000000')
+), (
+    'Bent over Row',
+    (select id from auth.users where instance_id = '00000000-0000-0000-0000-000000000000')
+);
+
+insert into workout (
+    created_by,
+    name,
+    nickname
+) values (
+    (select id from auth.users where instance_id = '00000000-0000-0000-0000-000000000000'),
+    'Workout A',
+    'Upper'
+), (
+    (select id from auth.users where instance_id = '00000000-0000-0000-0000-000000000000'),
+    'Workout B',
+    'Lower'
+);
+
+insert into workout_exercise (
+    workout_id,
+    exercise_id,
+    created_by,
+    sets,
+    reps,
+    weight
+) values (
+    (select id from workout where name = 'Workout A'),
+    (select id from exercise where name = 'Bench Press'),
+    (select id from auth.users where instance_id = '00000000-0000-0000-0000-000000000000'),
+    3,
+    '5',
+    '100kg'
+),
+(
+    (select id from workout where name = 'Workout A'),
+    (select id from exercise where name = 'Shoulder Press'),
+    (select id from auth.users where instance_id = '00000000-0000-0000-0000-000000000000'),
+    3,
+    '5',
+    '100kg'
+), (
+    (select id from workout where name = 'Workout A'),
+    (select id from exercise where name = 'Bent over Row'),
+    (select id from auth.users where instance_id = '00000000-0000-0000-0000-000000000000'),
+    3,
+    '5',
+    '100kg'
+),
+(
+    (select id from workout where name = 'Workout B'),
+    (select id from exercise where name = 'Squat'),
+    (select id from auth.users where instance_id = '00000000-0000-0000-0000-000000000000'),
+    3,
+    '5',
+    '100kg'
+),
+(
+    (select id from workout where name = 'Workout B'),
+    (select id from exercise where name = 'Deadlift'),
+    (select id from auth.users where instance_id = '00000000-0000-0000-0000-000000000000'),
+    3,
+    '5',
+    '100kg'
+);
+
+insert into workout_week (
+    week,
+    created_by
+) values (
+    1,
+    (select id from auth.users where instance_id = '00000000-0000-0000-0000-000000000000')
+), (
+    2,
+    (select id from auth.users where instance_id = '00000000-0000-0000-0000-000000000000')
+);
+
+insert into workout_day (
+    workout_week_id,
+    workout_id,
+    created_by,
+    day
+) values (
+    (select id from workout_week where week = 1),
+    (select id from workout where name = 'Workout A'),
+    (select id from auth.users where instance_id = '00000000-0000-0000-0000-000000000000'),
+    1
+), (
+    (select id from workout_week where week = 1),
+    (select id from workout where name = 'Workout B'),
+    (select id from auth.users where instance_id = '00000000-0000-0000-0000-000000000000'),
+    2
+), (
+    (select id from workout_week where week = 2),
+    (select id from workout where name = 'Workout A'),
+    (select id from auth.users where instance_id = '00000000-0000-0000-0000-000000000000'),
+    1
+), (
+    (select id from workout_week where week = 2),
+    (select id from workout where name = 'Workout B'),
+    (select id from auth.users where instance_id = '00000000-0000-0000-0000-000000000000'),
+    2
+);
+
+insert into workout_cycle (
+    workout_week_id,
+    created_by,
+    cycle
+) values (
+    (select id from workout_week where week = 1),
+    (select id from auth.users where instance_id = '00000000-0000-0000-0000-000000000000'),
+    1
+), (
+    (select id from workout_week where week = 2),
+    (select id from auth.users where instance_id = '00000000-0000-0000-0000-000000000000'),
+    1
+);
