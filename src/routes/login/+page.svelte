bdefcd0db70317f80c1cd11db46e7ce59efab5cd
Steven Bister <stevebister@gmail.com>
2024-04-12 19:43:52 +0100

    refactor: replace test inputs with input component

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { enhance } from '$app/forms';
-	import Input from '$lib/components/Input.svelte';
+	import Input from '~/src/lib/components/Input/Input.svelte';
 	import type { ActionData } from './$types';
 
 	export let form: ActionData;
@@ -9,12 +9,17 @@
 <h1>Login</h1>
 
 <form method="post" use:enhance>
-	<Input label="Email" name="email" value={form?.email ?? ''} required />
+	<Input
+		id="email"
+		name="email"
+		type="email"
+		label="Email"
+		value={form?.email ?? ''}
+		placeholder="user@email.com"
+		required
+	/>
 
-	<label
-		>password
-		<input type="password" name="password" required />
-	</label>
+	<Input id="password" name="password" type="password" label="Password" required />
 
 	<button>Log in</button>
 
