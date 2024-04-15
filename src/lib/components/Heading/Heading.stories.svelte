abcef465524ecc8d8d5cf97641580f35a74d80a9
Steven Bister <stevebister@gmail.com>
2024-04-15 17:08:27 +0100

    feat: add generic heading component

---
+++ src/lib/components/Heading/Heading.stories.svelte
@@ -0,0 +1,29 @@
+<script lang="ts" context="module">
+	import { Story, Template } from '@storybook/addon-svelte-csf';
+	import type { Meta } from '@storybook/svelte';
+	import Heading from './Heading.svelte';
+
+	export const meta = {
+		title: 'Heading',
+		component: Heading,
+		tags: ['autodocs'],
+		argTypes: {},
+	} satisfies Meta<Heading>;
+
+	const args = {
+		title: 'Title',
+		subTitle: 'Subtitle',
+	};
+</script>
+
+<Template let:args>
+	<Heading {...args} />
+</Template>
+
+<Story name="Primary" args={{ ...args }} />
+
+<Story name="Large" args={{ ...args, size: 'lg' }} />
+
+<Story name="Small" args={{ ...args, size: 'sm' }} />
+
+<Story name="Heading level 2" args={{ ...args, level: 2 }} />
---
+++ types/components.ts
@@ -0,0 +1,3 @@
+export type HeadingLevel = 1 | 2 | 3 | 4 | 5 | 6;
+
+export type Size = 'sm' | 'md' | 'lg';
