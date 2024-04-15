08f421f173671badf73bc244ae2103e93828367d
Steven Bister <stevebister@gmail.com>
2024-04-15 19:29:43 +0100

    feat: add icon component

---
+++ src/lib/components/Icon/Icon.stories.svelte
@@ -0,0 +1,22 @@
+<script lang="ts" context="module">
+	import { Story, Template } from '@storybook/addon-svelte-csf';
+	import type { Meta } from '@storybook/svelte';
+	import Icon from './Icon.svelte';
+
+	export const meta = {
+		title: 'Icon',
+		component: Icon,
+		tags: ['autodocs'],
+		argTypes: {},
+	} satisfies Meta<Icon>;
+
+	const args = {
+		sprite: 'arrow-right',
+	};
+</script>
+
+<Template let:args>
+	<Icon {...args} />
+</Template>
+
+<Story name="Primary" args={{ ...args }} />
