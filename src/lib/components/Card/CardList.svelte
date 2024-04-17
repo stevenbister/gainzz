2120b8680ee32fcb5b66ca4667079a4b663d0b7f
Steven Bister <stevebister@gmail.com>
2024-04-17 20:26:28 +0100

    feat: add simple card list component

---
+++ src/lib/components/Card/CardList.svelte
@@ -0,0 +1,13 @@
+<div {...$$restProps} class={`card-list ${$$restProps.class ? $$restProps.class : ''}`}>
+	<slot />
+</div>
+
+<style lang="scss">
+	.card-list {
+		--_card-list-gap: var(--space-1);
+
+		display: flex;
+		flex-direction: column;
+		gap: var(--_card-list-gap);
+	}
+</style>
