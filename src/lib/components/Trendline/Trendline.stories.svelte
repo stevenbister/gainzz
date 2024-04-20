229ba18e1291add5f85ce264619e3a700e854fa2
Steven Bister <stevebister@gmail.com>
2024-04-20 17:44:12 +0100

    feat: add trendline component

---
+++ src/lib/components/Trendline/Trendline.stories.svelte
@@ -0,0 +1,44 @@
+<script lang="ts" context="module">
+	import { Story, Template } from '@storybook/addon-svelte-csf';
+	import type { Meta } from '@storybook/svelte';
+	import Trendline from './Trendline.svelte';
+
+	export const meta = {
+		title: 'Trendline',
+		component: Trendline,
+		tags: ['autodocs'],
+		argTypes: {},
+	} satisfies Meta<Trendline>;
+
+	const args = {
+		dir: 'up',
+		diff: 2,
+	};
+</script>
+
+<Template let:args>
+	<Trendline {...args} />
+</Template>
+
+<Story
+	name="Primary"
+	args={{
+		...args,
+	}}
+/>
+
+<Story
+	name="Up"
+	args={{
+		...args,
+	}}
+/>
+
+<Story
+	name="Down"
+	args={{
+		...args,
+		dir: 'down',
+		diff: -2,
+	}}
+/>
---
+++ src/lib/components/Trendline/Trendline.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+	import type { Trend } from '~/types/components';
+	import Icon from '../Icon/Icon.svelte';
+
+	export let dir: Trend;
+	export let diff: number;
+</script>
+
+<span {...$$restProps} class={`trendline ${dir} ${$$restProps.class ? $$restProps.class : ''}`}>
+	<Icon sprite={`trend-${dir}`} class="trendline-icon" />
+
+	<span class="trendline-diff">{diff}</span>
+</span>
+
+<style lang="scss">
+	.trendline {
+		--_trendline-gap: var(--space--3);
+		--_trendline-icon-size: var(--fs-2);
+		--_trendline-icon-color-up: var(--color-success);
+		--_trendline-icon-color-down: var(--color-error);
+
+		display: flex;
+		align-items: center;
+		gap: var(--_trendline-gap);
+
+		:global(.trendline-icon) {
+			font-size: var(--_trendline-icon-size);
+		}
+
+		&.up :global(.trendline-icon) {
+			color: var(--_trendline-icon-color-up);
+		}
+
+		&.down :global(.trendline-icon) {
+			color: var(--_trendline-icon-color-down);
+		}
+	}
+
+	.trendline-diff {
+		font-variant-numeric: tabular-nums;
+	}
+</style>
