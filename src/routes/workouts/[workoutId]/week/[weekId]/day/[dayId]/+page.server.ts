49c8b6597694ae5271f7edc8a4980ef693a0468d
Steven Bister <stevebister@gmail.com>
2024-05-19 14:14:30 +0100

    feat: display list of exercises on the day page

--- src/routes/workouts/[workoutId]/week/[weekId]/day/[dayId]/+page.server.ts
+++ src/routes/workouts/[workoutId]/week/[weekId]/day/[dayId]/+page.server.ts
@@ -1,7 +1,17 @@
+import { getWorkoutDayById } from '$lib/queries/days';
+import { getWorkoutDayExercisesByWorkoutDayId } from '$lib/queries/exercises';
 import type { PageServerLoad } from './$types';
 
-export const load: PageServerLoad = async ({ params }) => {
+export const load: PageServerLoad = async ({ params, locals }) => {
+	const dayId = Number(params.dayId);
+
+	const workoutDayExercises = getWorkoutDayExercisesByWorkoutDayId(locals, {
+		workoutDayId: dayId,
+	});
+	const workoutDay = await getWorkoutDayById(locals, { workoutId: dayId });
+
 	return {
-		id: params.dayId,
+		workoutDay,
+		workoutDayExercises,
 	};
 };
--- src/routes/workouts/[workoutId]/week/[weekId]/day/[dayId]/+page.svelte
+++ src/routes/workouts/[workoutId]/week/[weekId]/day/[dayId]/+page.svelte
@@ -1,8 +1,39 @@
 <script lang="ts">
+	import Card from '$lib/components/Card/Card.svelte';
+	import CardList from '$lib/components/Card/CardList.svelte';
+	import ExerciseDayCard from '$lib/components/Card/ExerciseDayCard.svelte';
 	import Heading from '$lib/components/Heading/Heading.svelte';
+	import { padNumber } from '$lib/utils';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
 </script>
 
-<Heading title={`Day: ${data.id}`} />
+<Heading
+	title={`Day #${padNumber(data.workoutDay?.day)}`}
+	subTitle={data.workoutDay.workout?.nickname ?? ''}
+/>
+
+{#if data}
+	<CardList class="mt-3">
+		{#await data.workoutDayExercises}
+			<!-- eslint-disable-next-line @typescript-eslint/no-unused-vars -->
+			{#each Array(5) as _, index (index)}
+				<Card skeleton style="height: 112px" />
+			{/each}
+		{:then exercises}
+			{#if exercises.length === 0}
+				<p>No exercises found.</p>
+			{/if}
+
+			{#each exercises as exercise (exercise.id)}
+				<ExerciseDayCard
+					name={exercise.exercise?.name ?? 'No name set'}
+					weight={exercise.weight}
+					reps={exercise.reps}
+					sets={exercise.sets}
+				/>
+			{/each}
+		{/await}
+	</CardList>
+{/if}
